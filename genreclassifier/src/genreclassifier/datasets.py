04a37ce0509804dc4577456954e522ede705b73b
Austin Hwang <austinhwang18@gmail.com>
2025-11-04 16:57:47 -0500

    Add package skeleton

---
+++ genreclassifier/src/genreclassifier/datasets.py
@@ -0,0 +1,15 @@
+import os
+
+from pathlib import Path
+import pretty_midi
+
+
+
+
+def extract_genres_from_dataset(path: Path):
+    """
+    Extracts genres from the CD1 Genre Ground Truth dataset from https://www.tagtraum.com/msd_genre_datasets.html
+    at the given path on local machine
+    """
+    pass
+    
