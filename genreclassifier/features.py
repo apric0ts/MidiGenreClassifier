8ddd549648565540d83f4466e3e81c53ff315dcc
Austin Hwang <austinhwang18@gmail.com>
2025-11-05 14:21:20 -0500

    Add instruments to features, clean up type hints, add more examples

--- genreclassifier/features.py
+++ genreclassifier/features.py
@@ -1,23 +1,30 @@
 from dataclasses import dataclass
+import numpy as np
+import pretty_midi
 
+PROGRAM_ID_TO_INSTRUMENT_NAME = {
+    i: pretty_midi.program_to_instrument_name(i)
+    for i in range(128)
+}
 
 @dataclass
 class MidiFeatures:
     """
-    From a `PrettyMIDI` object, we can extract the following,
-    as well as some other information:
+    Features of a MIDI File. Easily extendible to include other types of features.
 
     tempo: estimated tempo of the song
-    key_signature_changes: list of key signatures throughout the song
-    time_signature_changes: list of time signatures throughout the song
+    instruments: list of instruments (represented as a list of ints)
+    key_signatures: list of key signatures throughout the song (represented as a list of ints)
+    time_signatures: list of time signatures throughout the song
     lyrics: whether the song has lyrics or not
     length: (rounded) length of the song in seconds
     """
-    tempo: int
-    key_signatures: list[str]
-    time_signatures: list[str]
+    tempo: np.float64
+    instruments: list[np.int64]
+    key_signatures: list[int]
+    time_signatures: list[tuple[int, int]]
     lyrics: bool
-    length: int
+    length: np.float64
 
 
 
@@ -28,11 +35,4 @@ class Track:
     """
     track_id: str
     genre: str
-    features: str
-
-
-def _hash_instruments():
-    """
-    Give each instrument name a unique integer for model training
-    """
-    
+    features: MidiFeatures
