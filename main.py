d6b3eb54d02128bdf7fd2dcce1e7e2b67e3e9229
Austin Hwang <austinhwang18@gmail.com>
2025-11-04 21:16:05 -0500

    Add some speedup / cacheing so don't have to run full code all the time. Main file has some silly examples on how to use the api

---
+++ main.py
@@ -0,0 +1,38 @@
+
+import os
+import genreclassifier as gc
+
+import numpy as np
+
+if __name__ == "__main__":
+
+    """
+    Example project structure:
+
+    outer-folder
+    - MidiGenreClassifier (our repo)
+    - MidiFiles (self explanatory)
+    - matches_scores.json (matching midi files to tracks in MSD)
+    - msd_tagtraum_cd1.cls (genres)
+    """
+    
+
+    # if we execute this from the project root:
+
+    # Project root directory
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    midi_files_path = os.path.join(parent_dir, "MidiFiles")
+    match_scores_path = os.path.join(parent_dir, "match_scores.json")
+    genres_path = os.path.join(parent_dir, "msd_tagtraum_cd1.cls")
+
+
+    all_track_info: list[gc.Track] = gc.get_all_track_information(midi_files_path, match_scores_path, genres_path)
+    
+
+    # Exmaples of how to use:
+    tempos = np.array([track.features.tempo for track in all_track_info])
+
+    print(tempos.min(), tempos.max(), tempos.mean())
+
+    
+        
