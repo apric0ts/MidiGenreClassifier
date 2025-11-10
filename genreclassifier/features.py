3548f0083e1fbb41e9fa2085d200c89ed0022937
Austin Hwang <austinhwang18@gmail.com>
2025-11-09 19:02:17 -0500

    Add function to turn features into a feature vector

--- genreclassifier/features.py
+++ genreclassifier/features.py
@@ -37,3 +37,51 @@ class Track:
     track_id: str
     genre: str
     features: MidiFeatures
+
+
+def extract_feature_vector(features: MidiFeatures) -> np.ndarray:
+    """
+    Turns `MidiFeatures` into an `np.ndarray` that can be fed into a NN for training.
+
+    Returns a vector of length 5 + 128 + 24 = 157
+
+    A design choice here is to use separate values for major and minor of the same key, i.e.
+    C major and C minor are represented differently as opposed to the same key. If our sample size is too small,
+    (perhaps there are very few songs that have C minor), then we can configure this encoding such that 
+    C major and C minor map to the same feature (so `key_vec = np.zeros(12), key_vec[k % 12] = 1.0`)
+
+    The final vector is of form: [tempo, length, lyrics, avg_num, avg_den, *instrument_vec, *key_vec],
+    where each element in the vector is of type `np.float64`
+    """
+    tempo = features.tempo
+    length = features.length
+    lyrics = 1.0 if features.lyrics else 0.0
+
+    # Instruments
+    # Convert list of ints representing instruments into a (one-hot) vector of length 128
+    instrument_vec = np.zeros(128)
+    for i in features.instruments:
+        instrument_vec[i] = 1.0
+
+    # Key signatures
+    # Convert list of ints representing key signatures into all possible keys
+
+    key_vec = np.zeros(24)
+    for k in features.key_signatures:
+        key_vec[k] = 1.0
+
+
+    # Time signatures: encode as average numerator/denominator (simple baseline)
+    if features.time_signatures:
+        numerators, denominators = zip(*features.time_signatures)
+        avg_num = np.mean(numerators)
+        avg_den = np.mean(denominators)
+    else: # if there's no valid key signatures found from the midi file
+        avg_num, avg_den = 4, 4  # default common time
+
+    # combine vectors, return
+    return np.concatenate([
+        [tempo, length, lyrics, avg_num, avg_den],
+        instrument_vec,
+        key_vec,
+    ])
