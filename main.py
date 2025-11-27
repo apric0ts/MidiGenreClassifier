8eb69fd71b9e43ddb54f34d179fe92e517601042
Austin Hwang <austinhwang18@gmail.com>
2025-11-27 12:01:58 -0500

    Add demo functionality

--- main.py
+++ main.py
@@ -3,65 +3,54 @@ import os
 from collections import Counter
 
 import numpy as np
+import torch
 
 import genreclassifier as gc
 
+MODEL_PATH = "trained_genre_classifier.pth"
+MIDI_PATH = "..." # insert here
 
 if __name__ == "__main__":
+    device = "cpu"
+    # 0. Load checkpoint
+    checkpoint = torch.load(MODEL_PATH, map_location=device, weights_only=False)
+    genre_to_idx = checkpoint['genre_to_idx']
+    idx_to_genre = checkpoint['idx_to_genre']
+    scaler = checkpoint['scaler']
 
-    """
-    Example project structure:
-
-    outer-folder
-    - MidiGenreClassifier (our repo)
-    - MidiFiles (self explanatory)
-    - matches_scores.json (matching midi files to tracks in MSD)
-    - msd_tagtraum_cd1.cls (genres)
-    """
-    # if we execute this from the repo root:
-
-    # Project root directory
-    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    midi_files_path = os.path.join(parent_dir, "lmd_matched")
-    match_scores_path = os.path.join(parent_dir, "match_scores.json")
-    genres_path = os.path.join(parent_dir, "msd_tagtraum_cd1.cls")
-
-
-    tracks: list[gc.Track] = gc.get_all_track_information(
-        midi_files_path, 
-        match_scores_path, 
-        genres_path, 
-        cache_path = "midi_features_cache_2.pkl",
-        files_walked_count=None # change this number to adjust the breadth of midi files we're analyzing. None means get all info
+    # Load model
+    model = gc.EnhancedTrackToGenreMLP(
+        input_dim=192,
+        num_classes=len(idx_to_genre),
+        hidden_dims=[256, 128, 64],
+        dropout=0.35,
+        use_batch_norm=True
     )
-    
 
-    # Examples of how to use:
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(device)
+    model.eval()
 
-    # Get min, max, mean of all track tempos
-    tempos = np.array([track.features.tempo for track in tracks])
-    print(tempos.min(), tempos.max(), tempos.mean())
+    # 2. Extract features from the MIDI file    
+    midi = gc.extract_data_from_midi(MIDI_PATH)
+    features: gc.MidiFeatures = midi.extract_feature_vector()
+    x = features.astype(np.float32) # (192,) vector
 
-    # Get number of instances of each genre across all tracks
-    genres = Counter([track.genre for track in tracks])
-    print(genres)
-    print(f"There are {len(genres)} unique genres across these tracks")
+    mask = gc.features.NORMALIZE_MASK
+    mask = gc.NORMALIZE_MASK
+    x_scaled = x.copy()
+
+    if scaler is not None:
+        x_scaled[mask] = scaler.transform(x[mask].reshape(1, -1))[0]
+
+    x = torch.tensor(x_scaled).unsqueeze(0).to(device)
+
+    # 3. Predict!
+    with torch.no_grad():
+        logits = model(x)
+        pred_idx = torch.argmax(torch.softmax(logits, dim=1)).item()
+    # 4. Map index to genre name
+    predicted_genre = idx_to_genre[pred_idx]
 
 
-    # Get number of instances of each instrument across all tracks
-    instrument_ids = []
-    for track in tracks:
-        instrument_ids.extend([int(x) for x in track.features.instruments]) # instruments represented as np.int64
-
-    instruments = Counter([gc.PROGRAM_ID_TO_INSTRUMENT_NAME[id] for id in instrument_ids])
-    print(instruments)
-    print(f"There are {len(instruments)} unique instruments across these tracks")
-
-    print("")
-    
-    
-    # Try out the nn stuff:
-    # dataset = gc.TrackDataset(tracks)
-
-    # input_dim = dataset.X.shape[1] # dataset.X is [num_tracks, num_features]
-    # model = gc.TrackToGenreMLP(input_dim=input_dim, num_classes=len(genres))
+    print("Predicted genre:", predicted_genre)
